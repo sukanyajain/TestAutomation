e6b66718b9914141026b3f968c387c50e0c4887a
sahusoft <sahusoft@192.168.175.1>
2022-08-12 10:57:11 +0530

    Added new change

--- SeleniumJavaNewFramework/src/test/java/seleniumwebelements.java
+++ SeleniumJavaNewFramework/src/test/java/seleniumwebelements.java
@@ -1,9 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class seleniumwebelements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+		
+		System.setProperty("webdriver.gecko.driver", projectPath+"/drivers/geckodriver/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		
+		//System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver/chromedriver.exe");
+				//WebDriver driver = new ChromeDriver();
+		
+         driver.get("http://google.com/");
+		
+		 driver.findElement(By.xpath("//input[@name='q']")).sendKeys("abcd");
+		 
+			
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			driver.close();
+			driver.quit();
+		
+		
 	}
 
 }
