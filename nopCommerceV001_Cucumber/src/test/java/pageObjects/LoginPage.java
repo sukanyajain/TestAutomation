671d965eefd5485d0b939cd6769c956f88069387
sukanyajain <41284072+sukanyajain@users.noreply.github.com>
2022-09-30 13:34:43 +0530

    CHANGES

--- nopCommerceV001_Cucumber/src/test/java/pageObjects/LoginPage.java
+++ nopCommerceV001_Cucumber/src/test/java/pageObjects/LoginPage.java
@@ -1,5 +1,54 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+	
+	public WebDriver driver;
+	
+	public  LoginPage(WebDriver rdriver)
+	{
+		
+		driver=rdriver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(id="Email")
+	WebElement txtemail;
+	
+	
+	@FindBy(id="password")
+	WebElement paswd;
+	
+	@FindBy(xpath="//input[@value='log in']")
+	WebElement btnlogin;
+	
+	@FindBy(linkText="Logout")
+	WebElement logout;
+	
+    public void setUsername(String uname) {
+	txtemail.clear();
+	txtemail.sendKeys(uname);
+}
+	
+    public void setPaswrd(String pswd) {
+	paswd.clear();
+	paswd.sendKeys(pswd);
+}
+    public void ClickLogin() {
+    	btnlogin.click();
+    }
+	
+    public void logout() {
+    	logout.click();
+    }
+	
+
+	
+	
+	
+	
 
 }
